34ea856c6192e11abdff252e76234b8679747df5
krishnanshuvar1204 <144117183+krishnanshuvar1204@users.noreply.github.com>
2025-05-02 18:30:31 +0530

    Add files via upload

---
+++ Exp 1/Address.java
@@ -0,0 +1,6 @@
+class Address{
+    public static void main(String[] args){
+        System.out.println("Krishnanshu Varshney");
+        System.out.println("Surya Nagar, Ghaziabad, Uttar Pradesh");
+    }
+}
---
+++ Exp 1/GradeSheet.java
@@ -0,0 +1,19 @@
+public class GradeSheet {
+    public static void main(String[] args) {
+        System.out.println("****************************************");
+        System.out.println("*\t\t  GRADE SHEET  \t\t *");
+        System.out.println("****************************************");
+
+        System.out.println("* Student Name\t\tMarks\tGrade\t *");
+        System.out.println("*--------------------------------------- *");
+
+        System.out.println("* Rahul \t\t85\tB\t *");
+        System.out.println("* Rohan \t\t92\tA\t *");
+        System.out.println("* Bhoomi\t\t74\tC\t *");
+        System.out.println("* Ipshita\t\t58\tD\t *");
+
+        System.out.println("****************************************");
+        System.out.println("*\t   End of Grade Sheet  \t\t *");
+        System.out.println("****************************************");
+    }
+}
